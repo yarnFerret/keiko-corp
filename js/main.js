daf81a96c96a83612f77f4f3c3f96a4d90e1fb25
FerretsNook <yarnferret@gmail.com>
2020-07-10 18:37:46 +0200

    bootstrap removed

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@ $(document).ready(function(){
 
 	//Navigation menu scrollTo
 	$('header nav ul li a').click(function(event){
-		event.preventDefault();
-		var section = $(this).attr('href');
-		var section_pos = $(section).position();
+	// 	event.preventDefault();
+	// 	var section = $(this).attr('href');
+	// 	var section_pos = $(section).position();
 
-		if(section_pos){
-			$(window).scrollTo({top:section_pos.top, left:'0px'}, 1000);
-		}
+	// 	if(section_pos){
+	// 		$(window).scrollTo({top:section_pos.top, left:'0px'}, 1000);
+	// 	}
 		
-	});
+	// });
 
-	$('.app_link').click(function(e){
-		event.preventDefault();
-		$(window).scrollTo({top:$("#hero").position().top, left:'0px'}, 1000);		
+	// $('.app_link').click(function(e){
+	// 	event.preventDefault();
+	// 	$(window).scrollTo({top:$("#hero").position().top, left:'0px'}, 1000);		
 	});
 
 
